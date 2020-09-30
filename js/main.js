72ac277502683b6b5099066630612039691497a8
David Copperfield <958621135@qq.com>
2020-09-30 12:20:39 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -101,7 +101,15 @@ $(function() {
             });
         });
     }
-    document.documentElement.addEventListener('gesturestart', function(event) {
-        event.preventDefault();
-    }, false);
 })
+window.onload = function() {
+        document.addEventListener('touchstart',
+            function(event) {
+                if (event.touches.length > 1) {
+                    event.preventDefault()
+                }
+            })
+        document.addEventListener('gesturestart',
+            function(event) {
+                event.preventDefault()
+            })
